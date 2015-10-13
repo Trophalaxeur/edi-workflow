55fd70fd93adf1d90a9d4f486a44a473d0df27e3
Jan Vereecken <janvereecken@clubit.be>
2015-10-13 11:14:55 +0200

    FIX: Format code and remove superfluous lines in edi_routes_orders ADDED:  Better description of edi_routes_orders

--- edi_routes_orders/__openerp__.py
+++ edi_routes_orders/__openerp__.py
@@ -1,6 +1,17 @@
 {
     'name': 'edi_routes_orders',
     'summary': 'Edifact ORDERS communication using the EDI framework',
+    'description' : """
+Edifact ORDERS communication using the EDI framework
+====================================================
+
+This module implements routes for the following implementations.
+
+    * ORDERS 93A
+    * ORDERS 96A
+
+The input format is not the original EDIFACT format, but rather the translation of this format. The original format is processed by bots (http://bots.sourceforge.net).
+    """,
     'version': '1.0',
     'category': 'EDI Tools',
     'author': 'Clubit BVBA',
