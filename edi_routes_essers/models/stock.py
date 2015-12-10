86829d3d7f2b95d7dcefb8c96e1cfddd03f0b431
Jan Vereecken <janvereecken@clubit.be>
2015-12-10 10:41:10 +0100

    FIX: Remove 2-step-delivery workflow from document creation. FIX: Do not process pickings in state done.

--- edi_routes_essers/models/stock.py
+++ edi_routes_essers/models/stock.py
@@ -39,21 +39,10 @@ class stock_picking(models.Model):
             raise except_orm(_('Invalid pickings in selection!'), _('The following pickings are invalid, please remove from selection. %s') % (map(lambda record: record.name, invalid_pickings)))
 
         for picking in valid_pickings:
-            out_pickings = self.env['stock.picking'].search([('group_id', '=', picking.group_id.id), ('picking_type_id', '=', 2)])
-            #if len(out_pickings) != 1:
-            #    raise except_orm(_('EDI send failed!', _('EDI send failed because the number of related OUT pickings was not one')))
-            for out_picking in out_pickings:
-                content = out_picking.edi_export_essers(out_picking, None)
-                result = self.env['edi.tools.edi.document.outgoing'].create_from_content(out_picking.name, content, partner_id.id, 'stock.picking', 'send_edi_export_essers', type='XML')
+            content = picking.edi_export_essers(picking, None)
+                result = self.env['edi.tools.edi.document.outgoing'].create_from_content(picking.name, content, partner_id.id, 'stock.picking', 'send_edi_export_essers', type='XML')
                 if not result:
                     raise except_orm(_('EDI creation failed!', _('EDI processing failed for the following picking %s') % (picking.name)))
-
-            transfer the transit picking
-            if not picking.pack_operation_ids:
-               picking.do_prepare_partial()
-
-            if picking.pack_operation_ids:
-               picking.do_transfer()
         return True
 
     @api.model
@@ -224,6 +213,9 @@ class stock_picking(models.Model):
         delivery = self.search([('name', '=', content['DELIVERY'].replace('_', '/'))], limit=1)
         if not delivery:
             raise EdiValidationError('Could not find the referenced delivery: {!s}.'.format(content['DELIVERY']))
+
+        if delivery.state == 'done':
+            raise EdiValidationError("Delivery already transfered.")
 
         lines_without_sequence = [ml for ml in delivery.move_lines if not ml.edi_sequence]
         if lines_without_sequence:
