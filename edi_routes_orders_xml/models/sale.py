05feb0632992089ed7362ad085b867e80f63d57e
Odoo <odoo@clubit.be>
2016-11-16 06:18:20 +0000

    ADDED: Orders XML

---
+++ edi_routes_orders_xml/models/sale.py
@@ -0,0 +1,90 @@
+import datetime
+import logging
+import xml.etree.cElementTree as ET
+import xmltodict
+
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm
+from openerp.addons.edi_tools.models.exceptions import EdiIgnorePartnerError, EdiValidationError
+
+_logger = logging.getLogger(__name__)
+
+class sale_order(models.Model):
+    _inherit = "sale.order"
+    
+    @api.model
+    def edi_import_order_xml_validator(self, document_ids):
+        _logger.debug("Validating XML document")
+
+        # Read the EDI Document
+        edi_db = self.env['edi.tools.edi.document.incoming']
+        document = edi_db.browse(document_ids)
+        document.ensure_one()
+
+        # Convert the document to JSON
+        # import pdb; pdb.set_trace()
+        try:
+            content = xmltodict.parse(document.content)
+            #valid = content[InitialPurchaseOrder]
+            #valid = content[InitialPurchaseOrder][PurchaseOrderHeader]
+        except Exception:
+            raise EdiValidationError('Content is not valid XML or the structure deviates from what is expected.')
+
+        if not 'InitialPurchaseOrder' in content:
+            _logger.debug("XML Order document invalid, no InitialPurchaseOrder segment")
+            raise EdiValidationError('XML Order document invalid, no InitialPurchaseOrder segment')
+
+        if not 'DestinationName1' in content['InitialPurchaseOrder']['PurchaseOrderHeader']['IdDestination']:
+            _logger.debug("XML Order document invalid, no Destination Name in IdDestination")
+            raise EdiValidationError('XML Order document invalid, no Destination Name in IdDestination')
+        else:
+            header = content['InitialPurchaseOrder']['PurchaseOrderHeader']
+            delivery_partner = self.env['res.partner'].search([('name','=',header['IdDestination']['DestinationName1'])])
+            if not delivery_partner:
+                raise EdiValidationError('Delivery Partner not found')
+
+        _logger.debug("XML Order document valid")
+        return True
+
+    @api.model
+    def receive_edi_import_order_xml(self, document_ids):
+        document = self.env['edi.tools.edi.document.incoming'].browse(document_ids)
+        document.ensure_one()
+        return self.edi_import_order_xml(document)
+
+    @api.model
+    def edi_import_order_xml(self, document):
+        content = xmltodict.parse(document.content)
+        header = content['InitialPurchaseOrder']['PurchaseOrderHeader']
+        delivery_partner = self.env['res.partner'].search([('name','=',header['IdDestination']['DestinationName1'])])
+        _logger.info("Partner Found: %s", delivery_partner.id)
+        
+        param = {}
+
+        param['partner_id'] = document.partner_id.id
+        param['partner_shipping_id'] = delivery_partner.id
+        param['partner_invoice_id'] = document.partner_id.id
+        param['client_order_ref'] = header['IdOrderData']['PurchaseOrderNumber'] + ' ' + header['IdOther']['Reference']
+        req_date = header['IdOther']['DeliveryDate'][6:] + '-' + header['IdOther']['DeliveryDate'][3:5] + '-' + header['IdOther']['DeliveryDate'][:2] + ' 05:00:00'
+        param['requested_date'] = req_date
+       
+        param['order_line'] = []
+        for line in content['InitialPurchaseOrder']['PurchaseOrderRows']['IdRows']:
+            if 'BarCode' in line:
+                detail = {}
+                detail['product_id'] =  self.env['product.product'].search([('ean13','=',line['BarCode'])]).id
+                detail['product_uom_qty'] = float(line['Quantity'])
+                order_line = []
+                order_line.extend([0])
+                order_line.extend([False])
+                order_line.append(detail)
+                param['order_line'].append(order_line)
+                _logger.info("Product Found: %s", detail['product_id'])
+            else:
+                _logger.info("No BarCode element in line")
+
+
+        sid = self.create(param)
+        so = self.browse(sid.id)[0]
+
+        return so.name
