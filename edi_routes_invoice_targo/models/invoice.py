eb6beac1ae886a0bb94d2677e62755cff1981455
Dimitri Verhelst <dimitriverhelst@clubit.be>
2018-06-06 22:09:57 +0200

    FIX: TARGO edi route UTF compatibility changes

--- edi_routes_invoice_targo/models/invoice.py
+++ edi_routes_invoice_targo/models/invoice.py
@@ -40,7 +40,7 @@ class account_invoice(models.Model, EDIMixin):
         edi_doc = ''
         grand_total = 0.0
         invoice_counter = 0
-
+        corp = '1   '
         for invoice in self:
             #field definition
             label = '10'
@@ -51,16 +51,19 @@ class account_invoice(models.Model, EDIMixin):
             else:
                 debtor = str(invoice.partner_id.targo_reference).ljust(10)
             debtor_type = '2'
-            invoice_name = str(invoice.number)[10:20].ljust(10)
+            invoice_name = str(invoice.number)[-10:].ljust(10)
             invoice_date = datetime.datetime.strptime(invoice.date_invoice, "%Y-%m-%d").strftime("%d%m%Y")
             value_date = datetime.datetime.strptime(invoice.date_invoice, "%Y-%m-%d").strftime("%d%m%Y")
-            amount_total = str(('%.2f' % invoice.amount_total).replace('.',',')).ljust(14)
+            amount_total = str(('%.2f' % invoice.amount_total).replace('.',',')).rjust(14)
             grand_total += invoice.amount_total
             invoice_type = '+' #+ for invoices, - for credit notes 
-            amount_tax = str(('%.2f' % invoice.amount_tax).replace('.',',')).ljust(14)
+            amount_tax = str(('%.2f' % invoice.amount_tax).replace('.',',')).rjust(14)
             currency = invoice.currency_id.name
             termcode = ''.ljust(3)
-            termdays = str(invoice.payment_term_id.line_ids[0].days).ljust(3)
+            if invoice.payment_term_id:
+                termdays = str(invoice.payment_term_id.line_ids[0].days).ljust(3)
+            else:
+                termdays = str("28").ljust(3)
             maturity_date = datetime.datetime.strptime(invoice.date_due, "%Y-%m-%d").strftime("%d%m%Y")
             discount_1_days = str(invoice.partner_id.discount_1_days).ljust(3)
             discount_2_days = str(invoice.partner_id.discount_2_days).ljust(3)
@@ -72,16 +75,16 @@ class account_invoice(models.Model, EDIMixin):
                 endofmonth = '0'
             else:
                 endofmonth = '1'
-            endofmonthdays = str(invoice.payment_term_id.line_ids[0].days).ljust(3)
-            transfer_days = '2  '
-            dname = invoice.partner_id.name.ljust(40)
-            dstreet = invoice.partner_id.street.ljust(40)
-            dzip = str(invoice.partner_id.zip).ljust(10)
-            dcity = invoice.partner_id.city.ljust(40)
+            endofmonthdays = '   '
+            transfer_days = '   '
+            dname = invoice.partner_id.name[:30].ljust(40)
+            dstreet = invoice.partner_id.street[:30].ljust(40)
+            dzip = invoice.partner_id.zip[:10].ljust(10)
+            dcity = invoice.partner_id.city[:35].ljust(40)
             dcountry = invoice.partner_id.country_id.code.ljust(2)
-            finremark = ''
-            submitter = str(invoice.user_id.id).ljust(5)
-            
+            finremark = '   '
+            submitter = ''.ljust(5)
+
             #append lines
             line_content = label + corp + client + debtor + debtor_type + invoice_name + invoice_date + value_date + amount_total + invoice_type + amount_tax +currency + termcode + termdays + maturity_date + discount_1_perc + discount_1_days + discount_2_perc + discount_2_days + discount_3_perc + discount_3_days + endofmonth + endofmonthdays + transfer_days + dname + dstreet + dzip + dcity + dcountry + finremark + submitter
             edi_doc = edi_doc +line_content + '\n'
@@ -89,8 +92,12 @@ class account_invoice(models.Model, EDIMixin):
         
         #append final line
         label = '30'
-        footer = label + corp + client + ''.ljust(11) + str(invoice_counter).ljust(26) + str(grand_total).ljust(218) + submitter
+        grand_total = str(grand_total).replace('.',',')
+        footer = label + corp + client + ''.ljust(11) + str(invoice_counter).ljust(26) + grand_total.ljust(218) + submitter
         edi_doc = edi_doc + footer + '\n'
 
+        #remove illegal characters
+        edi_doc = edi_doc.replace('ä', 'a').replace('Ä', 'A').replace('ö', 'o').replace('Ö', 'O').replace('ü', 'u').replace('Ü', 'U').replace('ß', 's').replace('ç', 'c').replace('Ç', 'C').replace('â', 'a').replace('Ğ', 'G').replace('ğ', 'g').replace('İ', 'I').replace('î', 'i').replace('Ş', 'S').replace('ş', 's')
+        
         #return consolidated result
         return edi_doc
