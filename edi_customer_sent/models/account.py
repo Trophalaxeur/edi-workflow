c6a9162cc6ec14f26abb891a2d8fd3e32e7e4863
Dimitri Verhelst <dimitriverhelst@clubit.be>
2018-06-10 12:53:10 +0200

    FIX: adapt customer sent method to listen to multiple routes

--- edi_customer_sent/models/account.py
+++ edi_customer_sent/models/account.py
@@ -8,14 +8,17 @@ class account_invoice(models.Model):
     def _function_customer_sent_get(self):
         edi_db = self.env['edi.tools.edi.document.outgoing']
         flow_db = self.env['edi.tools.edi.flow']
-        flow_id = flow_db.search([('model', '=', 'account.invoice'), ('method', '=', 'send_edi_export_invoic')], limit=1)
-        for invoice in self:
-            edi_docs = edi_db.search([('flow_id', '=', flow_id), ('reference', '=', invoice.number)])
-            if not edi_docs:
-                continue
-            edi_docs.sorted(key=lambda x: x.create_date, reverse=True)
-            invoice.customer_sent = edi_docs[0].create_date
+        flow_ids = flow_db.search([('model', '=', 'account.invoice'), 
+            ('method', 'in', ['send_edi_export_invoic'])
+            ])
+        for flow_id in flow_ids:
+            for invoice in self:
+                edi_docs = edi_db.search([('flow_id', '=', flow_id.id), ('reference', '=', invoice.number)])
+                if not edi_docs:
+                    continue
+                edi_docs.sorted(key=lambda x: x.create_date, reverse=True)
+                invoice.customer_sent = edi_docs[0].create_date
 
 
 
-    customer_sent = fields.Datetime(compute=_function_customer_sent_get, string='Customer Sent')
+    customer_sent = fields.Datetime(string='Customer Sent')
