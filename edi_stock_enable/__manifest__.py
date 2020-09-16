cf76541ae4258b8e6cb3aabadcbb31ca2fdde439
Florian Lefevre <contact@flefevre.fr>
2020-09-16 15:11:52 +0200

    convert edi_stock_enable to v13

---
+++ edi_stock_enable/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#    Author: Florian Lefevre
+#    Original Author: Jan Vereecken
+#    Source: http://www.clubit.be
+#    Copyright 2015 Clubit BVBA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+{'name': 'EDI for stock',
+ 'summary': 'Enable sending EDI for stock objects',
+ 'version': '13.0.1',
+ 'author': "Florian Lefevre / Clubit BVBA",
+ 'category': 'Warehouse Management',
+ 'license': 'AGPL-3',
+ 'images': [],
+ 'depends': ['edi_tools','stock'],
+ 'data': [
+     'wizards/picking.xml',
+ ],
+ 'auto_install': False,
+ 'installable': True,
+ }
